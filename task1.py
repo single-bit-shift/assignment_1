1c1a677b4347cb5a8c5a883fe9ac4754487edba1
single-bit-shif <josarratukullam@gmail.com>
2026-01-08 20:54:03 +0530

    initial commit

---
+++ task1.py
@@ -0,0 +1,12 @@
+n1=int(input("Enter the first number:\t"))
+n2=int(input("Enter the second number:\t"))
+add=n1+n2
+sub=n1-n2
+pdt=n1*n2
+div=n1/n2
+print("\n")
+print("Basic Operations:\n")
+print("Addition:",add,"\n")
+print("Subtraction:",sub,"\n")
+print("Multiplication:",pdt,"\n")
+print("Division:",round(div,2),"\n")
